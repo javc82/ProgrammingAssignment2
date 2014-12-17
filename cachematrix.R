98a66999d0ac1a058cda1482aa17933d08f491f0
javc82 <cantone@gmail.com>
2014-12-17 09:43:51 -0500

    Update cachematrix.R
    Second function added to get inverse

--- cachematrix.R
+++ cachematrix.R
@@ -7,7 +7,7 @@
 # - set the value of the mean
 # - get the value of the mean
 
-## Our first assigned task was to create makeCacheMatrix.
+## Our first assigned task was to create makeCacheMatrix, using the same four goals (though adapted from above).
 ## makeCacheMatrix creates a special matrix object that can cache the inverse
 
 ## The assignment started us off with: 
@@ -39,8 +39,37 @@ makeCacheMatrix <- function(x = matrix()) {
   get_inverse <- function() inv
   
   # Finally, we are returning a list of what we did above 
-  list(set = set, get = get,
-       set_inverse = set_inverse,
-       get_inverse = get_inverse)
+  list(set = set, get = get, set_inverse = set_inverse, get_inverse = get_inverse)
   
+}
+
+# The second function for the assignment calculates the inverse of
+# the matrix that we created in the first part of this assignment.
+# It first will check to see if the inverse was already calculated.
+# If it has been, it will use the cache and skip the math here, which
+# is the benefit of such a function. If the math computation was not
+# done before, it will calculate the inverse and set the value through
+# the set_inverse function provided. 
+
+  # First, the function will check and see if the inverse 
+  # is already in the cache.
+cacheSolve <- function(x, ...) {
+  inv <- x$get_inverse()
+  if(!is.null(inv)) {
+    message("getting cached inverse")
+    return(inv)
+  }
+  
+  # If not, we then get the matrix. 
+  data <- x$get()
+  
+  # and then we must calculate the inverse
+  inv <- solve(data, ...)
+  
+  # The inverse of the matrix is then put into the cache
+  x$set_inverse(inv)
+  
+  # Then, 'inv' gives us the result, which, as requested, should be
+  # "a matrix that is the inverse of 'x'.
+  inv
 }
